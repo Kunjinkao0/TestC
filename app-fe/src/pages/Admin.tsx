1f98a8e5b42dea1761728b9968c0b9b5f20654af
willi <zwj.nbsp@gmail.com>
2024-04-16 13:33:17 +0800

    Remove unused input field in Admin page

--- app-fe/src/pages/Admin.tsx
+++ app-fe/src/pages/Admin.tsx
@@ -3,7 +3,7 @@ import './Admin.scss';
 function AdminPage() {
   return (
     <div className="App">
-      <input type="text" placeholder="Search..." style={{ width: '80%', maxWidth: '400px', textAlign: 'center', margin: 'auto', marginTop: '20vh' }} />
+
     </div>
   );
 }
